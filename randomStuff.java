8b930ed1a735811697b52b88dfb6facd0ba1535b
Jennifer Esmay <jennifer.esm@lvjusd.k12.ca.us>
2018-12-03 13:11:02 -0800

    12/3/18

---
+++ randomStuff.java
@@ -0,0 +1,15 @@
+
+public class randomStuff
+{
+    
+    public static void main(){
+        
+        for(int i=0; i<10; i++){
+        //cast a random number to an integer 
+        
+        double age = (int)(Math.random()*8) + 367;
+        
+        System.out.println("the number is " + age);
+       }
+    }
+}
